464e7fce3a3efcde48f20b95431f0c56d29c1f76
primayazadi <primayazadi10@gmail.com>
2023-03-19 14:05:48 +0700

    l

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public class HaloGeneric
+    {
+        public static void sapaUser<T>(T inputuser)
+        {
+            Console.WriteLine("Hallo user " + inputuser);
+        }
+    }
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        HaloGeneric.sapaUser<string>("prima");
+        DataGeneric<string> nim = new DataGeneric<string>("1302210001");
+        nim.PrintData();
+    }
+}
